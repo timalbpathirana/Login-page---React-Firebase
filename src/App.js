d1695b37e510428b915ceb25e84d6aded15de8e5
Timal Pathirana <timalbathiya1995@gmail.com>
2021-04-23 17:51:27 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import UserLogin from "./Components/UserLogin";
+import Hero from "./Components/Hero";
+import fire from "./fire";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        switch (error.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(error.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(error.message);
+            break;
+          default:
+            break;
+        }
+      });
+  };
+
+  const handleSignUp = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        switch (error.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(error.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(error.message);
+            break;
+          default:
+            break;
+        }
+      });
+  };
+
+  const handleLogOut = () => {
+    fire.auth().signOut();
+    setUser("");
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <Hero handleLogOut={handleLogOut} />
+      ) : (
+        <UserLogin
+          email={email}
+          password={password}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+          handleSignUp={handleSignUp}
+          hasAccount={hasAccount}
+          setHasAccount={setHasAccount}
+          emailError={emailError}
+          passwordError={passwordError}
+        ></UserLogin>
+      )}
     </div>
   );
 }
